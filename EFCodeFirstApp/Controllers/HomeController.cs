a5512141737412f68caf08103e9f708b43529b4c
sharadaade <aadesharad@gmail.com>
2024-12-10 11:38:27 +0530

    Add project files.

---
+++ EFCodeFirstApp/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using EFCodeFirstApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFCodeFirstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        StudentContext db = new StudentContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            //var data = db.Students.ToList();
+            var data = db.Students.AsNoTracking().ToList();
+            return View(data);
+        }
+    }
+}
---
+++ EFCodeFirstApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EFCodeFirstApp.MvcApplication" Language="C#" %>
